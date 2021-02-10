53df4ad506d2afde958d2dac98338edc39cd7e78
denamyte <denamyte@gmail.com>
2021-02-11 01:54:18 +0200

    2021-02-11: Stage 1/4

--- Readability Score/task/src/previous/Main1.java
+++ Readability Score/task/src/previous/Main1.java
@@ -1,9 +1,9 @@
-package readability;
+package previous;
 
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main1 {
+    public static void main_(String[] args) {
         String text = new Scanner(System.in).nextLine();
         System.out.println(text.length() > 100 ? "HARD" : "EASY");
     }
